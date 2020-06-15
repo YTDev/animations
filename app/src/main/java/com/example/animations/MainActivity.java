630298040538a0fc34050d853bf6a4d0f46e2473
YTDev <belnisay@gmail.com>
2020-06-15 19:25:17 +0100

    Animations tutorial another commit

--- app/src/main/java/com/example/animations/MainActivity.java
+++ app/src/main/java/com/example/animations/MainActivity.java
@@ -5,18 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtHelloWorld,txtHiWorld;
+    TextView txtAndroid;
+    private Button btn;
     private boolean isHi=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtHelloWorld=findViewById(R.id.txtHelloWorld);
         txtHiWorld=findViewById(R.id.txtHiWorld);
+        txtAndroid=findViewById(R.id.txtAndroid);
+        btn=findViewById(R.id.btn);
+
+        txtHelloWorld.setX(2000);
+
+
+        txtAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                txtAndroid.animate().translationX(30).setDuration(3000);
+//                txtAndroid.animate().translationY(-10).setDuration(3000);
+//                txtAndroid.animate().rotation(180).setDuration(3000);
+//                txtAndroid.animate().rotationBy(30).setDuration(3000);
+//                txtAndroid.animate().rotationX(180).setDuration(3000);
+//                txtAndroid.animate().rotationX(360).translationY(1000).setDuration(5000);
+//                txtAndroid.animate().scaleX(0.3f).scaleY(0.3f).setDuration(3000);
+
+            }
+        });
 
         txtHelloWorld.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +59,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHelloWorld.animate().translationXBy(-2000).setDuration(3000);
+            }
+        });
     }
+
+
+
 }
